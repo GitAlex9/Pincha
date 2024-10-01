4302268aa3f54553d80b36e1b9ade75da42771da
IsabeliHubner <164574868+IsabeliHubner@users.noreply.github.com>
2024-10-01 19:11:44 -0300

    Background Music

---
+++ Assets/Audio/Audio Script/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+   [Header("---------- Audio Source ----------")]
+   [SerializeField] AudioSource musicSource;
+   [SerializeField] AudioSource SFXSource;
+
+   [Header("---------- Audio Clip ----------")]
+   public AudioClip background;
+   public AudioClip arrasta;
+   public AudioClip win;
+   public AudioClip death;
+
+
+   private void Start()
+   {
+    musicSource.clip = background;
+    musicSource.Play();
+   }
+
+   
+}
